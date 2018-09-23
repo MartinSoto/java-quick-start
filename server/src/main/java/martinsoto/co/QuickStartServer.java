78de8a2a71b363021fb6097b92be9e52091d7009
Martin Soto <donsoto@gmail.com>
2018-09-23 17:56:36 +0200

    Initial commit

---
+++ server/src/main/java/martinsoto/co/QuickStartServer.java
@@ -0,0 +1,11 @@
+package martinsoto.co;
+
+/**
+ * Hello world!
+ *
+ */
+public class QuickStartServer {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
