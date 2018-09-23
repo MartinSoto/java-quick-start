1490487aea32b40c65ca944f4a62bd46c28363a7
Martin Soto <donsoto@gmail.com>
2018-09-23 21:13:59 +0200

    Minimal hello-world socket server

--- server/src/main/java/martinsoto/co/QuickStartServer.java
+++ server/src/main/java/martinsoto/co/QuickStartServer.java
@@ -1,11 +1,33 @@
 package martinsoto.co;
 
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 /**
  * Hello world!
  *
  */
 public class QuickStartServer {
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+    public static void main(String[] args) throws IOException {
+        ServerSocket serverSocket;
+        int port = 4545;
+
+        serverSocket = new ServerSocket(port);
+        try {
+            Socket socket = serverSocket.accept();
+
+            try {
+                OutputStreamWriter writer = new OutputStreamWriter(socket.getOutputStream());
+                writer.write("Hello World!\n");
+                writer.flush();
+            } finally {
+                socket.close();
+            }
+        } finally {
+            serverSocket.close();
+        }
+
     }
 }
