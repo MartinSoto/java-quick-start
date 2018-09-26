5fce728dfe4de741ff68e293ef266e60c383e46e
Martin Soto <donsoto@gmail.com>
2018-09-26 22:50:50 +0200

    Open sockets for the task standard streams

--- server/src/main/java/martinsoto/co/quickstart/TaskHandler.java
+++ server/src/main/java/martinsoto/co/quickstart/TaskHandler.java
@@ -2,10 +2,61 @@ package martinsoto.co.quickstart;
 
 import java.io.IOException;
 import java.io.OutputStreamWriter;
+import java.net.ServerSocket;
 import java.net.Socket;
 
 public class TaskHandler extends Thread {
     private Socket socket;
+
+    private int stdInOutPort = 0;
+    private Socket stdInOutSocket;
+
+    private int stdErrPort = 0;
+    private Socket stdErrSocket;
+
+    private class StdInOutWaiter extends Thread {
+        private ServerSocket serverSocket;
+
+        StdInOutWaiter() throws IOException {
+            serverSocket = new ServerSocket(0);
+            stdInOutPort = serverSocket.getLocalPort();
+        }
+
+        @Override
+        public void run() {
+            try {
+                try {
+                    stdInOutSocket = serverSocket.accept();
+                } finally {
+                    serverSocket.close();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private class StdErrWaiter extends Thread {
+        private ServerSocket serverSocket;
+
+        StdErrWaiter() throws IOException {
+            serverSocket = new ServerSocket(0);
+            stdErrPort = serverSocket.getLocalPort();
+        }
+
+        @Override
+        public void run() {
+            try {
+                try {
+                    stdErrSocket = serverSocket.accept();
+                } finally {
+                    serverSocket.close();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
 
     TaskHandler(Socket socket) {
         this.socket = socket;
@@ -15,8 +66,11 @@ public class TaskHandler extends Thread {
     public void run() {
         OutputStreamWriter writer;
         try {
+            (this.new StdInOutWaiter()).start();
+            (this.new StdErrWaiter()).start();
+
             writer = new OutputStreamWriter(socket.getOutputStream());
-            writer.write("Hello World!\n");
+            writer.write(String.format("StdInOut: %d, StdErr %d\n", stdInOutPort, stdErrPort));
             writer.flush();
 
             writer.close();
